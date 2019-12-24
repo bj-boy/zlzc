87d8bdc91e265ad47bda839a19e2ef66cd1636b8
Lsr <457235804@qq.com>
2019-12-24 18:12:26 +0800

    tj 2019年12月24日 18点12分

---
+++ zlzc-admin/src/main/java/com/zlzc/modules/commodity/controller/CommodityAlbumController.java
@@ -0,0 +1,42 @@
+package com.zlzc.modules.commodity.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+/**
+ * @author LSR
+ * @email zlzc@example.com
+ * @date 2019-12-09 14:50:18
+ */
+@Api(value = "后台-商品-相册", tags = { "V1.0 commodityAlbum：后台-商品属性相册操作相关接口" })
+@RestController
+@RequestMapping("commodityAlbum")
+public class CommodityAlbumController {
+
+	// 商户或店铺相册列表(可分页 + 根据相册名称进行条件查询)
+	
+	// 新建相册
+	
+	// 修改相册
+	
+	// 指定相册的图片列表(可分页)
+	
+	// 转移指定图片到指定相册
+	
+	// 删除指定图片(可批量)
+	
+	// 上传图片到指定相册(可批量)
+	
+	// 纯上传图片(可批量)
+	
+	
+	/* ################# method ################# */
+	
+	
+	
+	
+	
+	
+}
